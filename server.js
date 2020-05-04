4a3f4a67c5d2120cfdd64a5da3e6c941378cab27
atticusrains@gmail.com <atticusrains@gmail.com>
2020-05-04 11:16:35 -0400

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const db = pgp(process.env.DATABASE_URL || "postgres://localhost:5432/yourproject");
+
+app.use(session({
+    store: new pgSession({
+      conString: process.env.DATABASE_URL || "postgres://localhost:5432/yourproject",
+    }),
+    key: 'user_sid',
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 10 * 10 * 6000000 },
+  }));
