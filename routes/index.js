5b850124efc3b79db71da701bb01d2c6f7c30c1f
atticusrains@gmail.com <atticusrains@gmail.com>
2020-05-08 12:43:01 -0400

    testing order api

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,6 @@ var conn = new jsforce.Connection({
   loginUrl : 'https://test.salesforce.com'
 });
 
-conn.login(process.env.SF_USERNAME, process.env.SF_PASSWORD, process.env.SF_SEC_TOKEN, function(err, userInfo) {
-  if (err) { return console.error(err); }
-  // Now you can get the access token and instance URL information.
-  // Save them to establish connection next time.
-  console.log(conn.accessToken);
-  console.log(conn.instanceUrl);
-  // logged in user property
-  console.log("User ID: " + userInfo.id);
-  console.log("Org ID: " + userInfo.organizationId);
-  // ...
-});
-
 const fs = require('fs');
 var products;
 
@@ -165,23 +153,37 @@ router.get('/submit', function (req, res, next) {
   });
   var body = JSON.stringify(order);
 
+  conn.login(process.env.SF_USERNAME, process.env.SF_PASSWORD, process.env.SF_SEC_TOKEN, function(err, userInfo) {
+    if (err) { return console.error(err); }
+    // Now you can get the access token and instance URL information.
+    // Save them to establish connection next time.
+    console.log(conn.accessToken);
+    console.log(conn.instanceUrl);
+    // logged in user property
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
+
+    conn.request({
+      method: 'post',
+      url: '/services/data/v48.0/commerce/sale/order',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body
+    }, function (err, res) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log("response: ", res);
+      // the response object structure depends on the definition of apex class
+    });
+    // ...
+  });
+
   console.log('fuck u');
   console.log('access Token:' + conn.accessToken);
 
-  conn.request({
-    method: 'post',
-    url: '/services/data/v48.0/commerce/sale/order',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body
-  }, function (err, res) {
-    if (err) {
-      return console.error(err);
-    }
-    console.log("response: ", res);
-    // the response object structure depends on the definition of apex class
-  });
+  
   //console.log('order: ' + JSON.stringify(order));
   /*
    * If company name field is blank order account id = household account.
