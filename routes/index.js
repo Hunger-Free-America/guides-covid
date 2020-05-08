b1c5f7f989442aadcdc890ef6c9eb2fde0c81152
atticusrains@gmail.com <atticusrains@gmail.com>
2020-05-08 11:50:47 -0400

    testing order api

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const jsforce = require('jsforce');
 
 var conn = new jsforce.Connection({
   loginUrl: 'https://test.salesforce.com',
-  instanceUrl: 'https://cs4.salesforce.com'
+  //instanceUrl: 'https://cs4.salesforce.com'
 });
 
 conn.login(process.env.SF_USERNAME, process.env.SF_PASSWORD, process.env.SF_SEC_TOKEN, function (err, res) {
