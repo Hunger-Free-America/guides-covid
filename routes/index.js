a0a0612724d7a08f4c06ea3c142f820c238c1e62
atticusrains@gmail.com <atticusrains@gmail.com>
2020-05-08 12:27:56 -0400

    testing order api

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,23 @@ const router = express.Router();
 const db = require('../database');
 const jsforce = require('jsforce');
 
+var jsforce = require('jsforce');
 var conn = new jsforce.Connection({
-  loginUrl: 'https://test.salesforce.com',
-  instanceUrl: 'https://cs2.salesforce.com'
+  // you can change loginUrl to connect to sandbox or prerelease env.
+  loginUrl : 'https://test.salesforce.com'
 });
 
-conn.login(process.env.SF_USERNAME, process.env.SF_PASSWORD, process.env.SF_SEC_TOKEN, function (err, res) {
-  console.log('logging in');
-  if (err) {
-    return console.error(err);
-  }
+conn.login(process.env.SF_USERNAME, process.env.SF_PASSWORD, process.env.SF_SEC_TOKEN, function(err, userInfo) {
+  if (err) { return console.error(err); }
+  // Now you can get the access token and instance URL information.
+  // Save them to establish connection next time.
   console.log(conn.accessToken);
-  
-}); 
-
-console.log('access token: ' + conn.accessToken);
+  console.log(conn.instanceUrl);
+  // logged in user property
+  console.log("User ID: " + userInfo.id);
+  console.log("Org ID: " + userInfo.organizationId);
+  // ...
+});
 
 const fs = require('fs');
 var products;
