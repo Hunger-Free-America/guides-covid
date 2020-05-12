fab4a9fa03873ebaaad9cdb645f9d4bd4e30b277
atticusrains@gmail.com <atticusrains@gmail.com>
2020-05-12 13:55:22 -0400

    I think I know how callbacks work now

--- routes/index.js
+++ routes/index.js
@@ -248,10 +248,12 @@ function checkAccount(accountName, callback) {
 }
 
 function checkContact(fname, lname, callback) {
-  console.log('checkContact');
+  console.log('checkContact...');
   var records = [];
   conn.query("SELECT Id, FirstName, LastName, Name FROM Contact WHERE FirstName LIKE '%" + fname + "%' AND LastName LIKE '%" + lname + "%'", function (err, result) {
+    console.log('error:' + err + "res: " + result)
     if (err) {
+      console.log(err);
       callback(err);
     }
     console.log("total contacts: " + result.totalSize);
