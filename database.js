c0fb215c800d57c29261e1f71e2b6c6896aa1a69
Atticus <atticusrains@gmail.com>
2021-03-11 10:24:45 -0500

    Database pooling

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl:{
-        rejectUnothorized = false
+        rejectUnothorized: false
     }
 })
 module.exports = {pool};
